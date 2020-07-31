7a65132e6a01d5ea41f99724c9553d725c60a8d3
AsifRKabir <asifrkabir20@gmail.com>
2020-07-31 09:43:26 +0600

    Completed

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let generatedPin = 0;
 let userPin = "";
+let tryLeft = 3;
 
 //Generating the pin
 const generateBtn = document.getElementById("btn-generate");
@@ -7,14 +8,19 @@ generateBtn.addEventListener("click", function () {
     generatedPin = Math.ceil(Math.random() * 10000);
     document.getElementById("generated-pin").value = generatedPin;
     document.getElementById("generated-pin").style.color = "white";
-    console.log(generatedPin);
+    userPin = "";
+    document.getElementById("user-input").value = userPin;
+    displayStyle("login-success", "none");
+    displayStyle("login-failed", "none");
+    document.getElementById("submit-btn").style.backgroundColor = "#495BC3";
+    document.getElementById("submit-btn").style.boxShadow = "0px 0px 20px rgb(27, 70, 151)";
 })
 
 //User entering pin
 const buttons = document.getElementsByClassName("button");
 for (let i = 0; i < buttons.length; i++) {
     let button = buttons[i];
-    button.addEventListener("click", function (event) {
+    button.addEventListener("click", function () {
         if (this.innerText == "C") {
             userPin = "";
             document.getElementById("user-input").value = userPin;
@@ -32,4 +38,37 @@ for (let i = 0; i < buttons.length; i++) {
             }
         }
     })
+}
+
+//User Submitting Pin
+const submitBtn = document.getElementById("submit-btn");
+submitBtn.addEventListener("click", function () {
+    if (userPin == generatedPin) {
+        displayStyle("login-success", "block");
+        document.getElementById("submit-btn").style.backgroundColor = "#17b978";
+        document.getElementById("submit-btn").style.boxShadow = "0px 0px 20px #17b978";
+    }
+    else {
+        displayStyle("login-failed", "block");
+        if (tryLeft > 1) {
+            tryLeft--;
+            document.getElementById("try-left").innerText = tryLeft;
+            document.getElementById("submit-btn").style.backgroundColor = "#ff304f";
+            document.getElementById("submit-btn").style.boxShadow = "0px 0px 20px #ff304f";
+        }
+        else {
+            document.getElementById("try-left").innerText = 0;
+            document.getElementById("submit-btn").disabled = true;
+            document.getElementById("submit-btn").style.backgroundColor = "gray";
+            document.getElementById("submit-btn").style.boxShadow = "none";
+            displayStyle("login-failed", "none");
+            displayStyle("no-try", "block");
+        }
+    }
+})
+
+
+//Function to set the display style of different elements
+function displayStyle(id, property){
+    return (document.getElementById(id).style.display = property)
 }
